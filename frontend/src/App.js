c2d697254cfaad9c1187becf40c61be2a6ea2624
Kyle Hua <kyleqhua@gmail.com>
2020-05-08 18:53:20 -0700

    kyle updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kyle is kool 
         </a>
       </header>
     </div>
