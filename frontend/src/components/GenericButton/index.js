31338157f0e37e87901c6b53364d441e64b4e826
Shannon Bonet <lolabonet@Lolas-MacBook-Pro.local>
2020-05-08 20:13:15 -0700

    button

--- frontend/src/components/GenericButton/index.js
+++ frontend/src/components/GenericButton/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './styles.css';
 
 class GenericButton extends React.PureComponent {
     constructor(props){super(props)}
     render(){
-        return(<button>{this.props.label}</button>);
+        return(<button className='g-button'>{this.props.label}</button>);
     }
 }
 
