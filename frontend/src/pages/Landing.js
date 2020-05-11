6e4739f67010f7bf0dafc96b90369f93d3dcfe03
Shannon Bonet <lolabonet@Lolas-MacBook-Pro.local>
2020-05-10 17:30:39 -0700

    ugh

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './Landing.css';
-import GenericButton from '../components/GenericButton/index.js';
- 
+import React from "react";
+import "./Landing.css";
+import GenericButton from "../components/GenericButton/index.js";
 
 function Landing() {
-    return (
-        <div className="Landing"> 
-            <header className="Landing-header"> 
-                <h1>we're strangers</h1>
-                <GenericButton label='start'></GenericButton>
-                <GenericButton label='instructions'></GenericButton>
-            </header> 
-        </div>
-    ); 
+  return (
+    <div className="Landing">
+      <header className="Landing-header">
+        <h1>we're strangers</h1>
+        <GenericButton label="start"></GenericButton>
+        <GenericButton label="instructions"></GenericButton>
+      </header>
+    </div>
+  );
 }
 
-export default Landing; 
+export default Landing;
