9ddc059d7fda83608afc91f894d840b0e389282a
Shannon Bonet <lolabonet@Lolas-MacBook-Pro.local>
2020-05-28 13:29:58 -0700

    added final page, restyled landing & instructions

---
+++ frontend/src/pages/LevelPage/Reflection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LevelLayout from "../../components/Levels/index.js";
+
+class Reflection extends React.Component {
+  render() {
+    return (
+      <div className={"reflection"}>
+        <LevelLayout title="level 3: reflection"></LevelLayout>
+      </div>
+    );
+  }
+}
+
+export default Reflection;
