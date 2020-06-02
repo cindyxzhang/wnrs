2d794b5b86d48b9e6b8e229f211d484c4368075b
Kyle Hua <kyleqhua@gmail.com>
2020-06-02 16:40:01 -0700

    fuck

---
+++ frontend/src/pages/Level1Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Level1Card.css';
+import GameCards from '../components/Cards/index.js';
+
+function Level1Card() {
+  return (
+    <div className="Level1Card">
+        <GameCards label='what was your first impression of me?'></GameCards>
+    </div>
+  );
+}
+
+export default Level1Card;
