43c335e3f06bca3af86ddee41f20ff618ff1ea87
Shannon Bonet <lolabonet@Lolas-MacBook-Pro.local>
2020-06-03 19:49:50 -0700

    email page centering + title

--- frontend/src/pages/Email/Email.js
+++ frontend/src/pages/Email/Email.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
 import GenericButton from "../../components/GenericButton/index.js";
+import "./Email.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    "& .MuiTextField-root": {
+    "& div.MuiTextField-root": {
       margin: theme.spacing(5),
-      width: 450,
+      width: "50ch",
       display: "flex",
+    },
+    "& div.MuiInputBase-root": {
+      borderRadius: "25px",
+    },
+    "& div.MuiFilledInput-underline:before": {
+      borderBottom: "none",
+    },
+    "& div.MuiFilledInput-underline:after": {
+      borderBottom: "none",
     },
   },
 }));
@@ -16,26 +28,30 @@ const useStyles = makeStyles((theme) => ({
 export default function BasicTextFields() {
   const classes = useStyles();
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField
-          id="outlined-basic"
-          label="Partner's Email"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <TextField
-          id="outlined-multiline-static"
-          label="Message"
-          multiline
-          rows={4}
-          variant="filled"
-        />
-      </div>
+    // need div tags that enclose entire return statement
+    <div className="layout">
+      <h1>pls werk</h1>
+      <form className={classes.root} noValidate autoComplete="off">
+        <div className="partner">
+          <TextField
+            id="outlined-basic"
+            label="Partner's Email"
+            variant="filled"
+          />
+        </div>
+        <div>
+          <TextField
+            id="outlined-multiline-static"
+            label="Message"
+            multiline
+            rows={4}
+            variant="filled"
+          />
+        </div>
+      </form>
       <div>
         <GenericButton label="send" className=""></GenericButton>
       </div>
-    </form>
+    </div>
   );
 }
